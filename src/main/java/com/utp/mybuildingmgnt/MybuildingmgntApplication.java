9f31e3868abfe79d71e8716522d83bbc8c4bd8e9
obedlandav1 <obed.landa.v@gmail.com>
2025-09-24 15:42:11 -0500

    Primer commit

---
+++ src/main/java/com/utp/mybuildingmgnt/MybuildingmgntApplication.java
@@ -0,0 +1,13 @@
+package com.utp.mybuildingmgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybuildingmgntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybuildingmgntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/mybuildingmgnt/MybuildingmgntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.mybuildingmgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybuildingmgntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
