e50c686df3af42b3b37384cf8eaf592b56539699
obedlandav1 <obed.landa.v@gmail.com>
2025-09-26 07:41:52 -0500

    Commit templates

--- src/main/java/com/utp/mybuildingmgnt/controller/MainController.java
+++ src/main/java/com/utp/mybuildingmgnt/controller/MainController.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class MainController {
 
     @GetMapping("/")
     public String home() {
-        return "home"; // busca en templates/index.html
+        return "home"; 
+    }
+
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login"; 
     }
 }
