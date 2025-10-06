62542d61469aceea3aae33bb15f3dbbff517c1cb
jeysonchs-coder <jeysonchs@gmail.com>
2025-10-05 22:44:41 -0500

    jeyson

--- src/main/java/com/utp/mybuildingmgnt/controller/MainController.java
+++ src/main/java/com/utp/mybuildingmgnt/controller/MainController.java
@@ -35,5 +35,9 @@ public class MainController {
     public String buildingedit() {
         return "layout/building-edit";
     }
-
+    
+    @GetMapping("/estate")
+    public String estate() {
+        return "layout/estate";
+    }
 }
